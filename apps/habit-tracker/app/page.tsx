f08c13311abb3d42aeff769a53e642c47b9bdbb5
aryanc381 <chauhanaryan381@gmail.com>
2026-02-20 23:14:37 +0530

    feat(ui): added home page.

--- apps/habit-tracker/app/page.tsx
+++ apps/habit-tracker/app/page.tsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="">
-      <p>hi</p>
+    <div className="flex flex-col items-center justify-center w-[100vw] h-[100vh]">
+      <p className="text-[5vw] font-mono tracking-[-0.5vw]">Habit Tracker</p>
+      <div className="text-[1.25vw]">
+        <p>The best solution to maximize and self critic your growth.</p>
+      </div>
     </div>
   );
 }
